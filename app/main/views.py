b79198aa28fe7536af8b7d43edda564de9e3770c
kekearif <kekearif@googlemail.com>
2017-08-07 23:33:07 +0800

    Fixed NEO symbol in JSON unwrap

--- app/main/views.py
+++ app/main/views.py
@@ -8,10 +8,10 @@ import json
 def index():
     with main.open_resource(app.config['JSON_PATH']) as infile:
         data = json.load(infile)
-        price_str = "%.2f" % data["RAW"]["ANS"]["USD"]["PRICE"]
-        low_str = "%.2f" % data["RAW"]["ANS"]["USD"]["LOW24HOUR"]
-        high_str = "%.2f" % data["RAW"]["ANS"]["USD"]["HIGH24HOUR"]
-        change = data["RAW"]["ANS"]["USD"]["CHANGE24HOUR"]
+        price_str = "%.2f" % data["RAW"]["NEO"]["USD"]["PRICE"]
+        low_str = "%.2f" % data["RAW"]["NEO"]["USD"]["LOW24HOUR"]
+        high_str = "%.2f" % data["RAW"]["NEO"]["USD"]["HIGH24HOUR"]
+        change = data["RAW"]["NEO"]["USD"]["CHANGE24HOUR"]
         change_str = "%.2f" % abs(change)
     return render_template('index.html', price=price_str, low=low_str, high=high_str, change=change_str, decrease=change<0)
 
@@ -20,9 +20,9 @@ def index():
 def price():
     with main.open_resource(app.config['JSON_PATH']) as infile:
         data = json.load(infile)
-        price_str = "%.2f" % data["RAW"]["ANS"]["USD"]["PRICE"]
-        low_str = "%.2f" % data["RAW"]["ANS"]["USD"]["LOW24HOUR"]
-        high_str = "%.2f" % data["RAW"]["ANS"]["USD"]["HIGH24HOUR"]
-        change = data["RAW"]["ANS"]["USD"]["CHANGE24HOUR"]
+        price_str = "%.2f" % data["RAW"]["NEO"]["USD"]["PRICE"]
+        low_str = "%.2f" % data["RAW"]["NEO"]["USD"]["LOW24HOUR"]
+        high_str = "%.2f" % data["RAW"]["NEO"]["USD"]["HIGH24HOUR"]
+        change = data["RAW"]["NEO"]["USD"]["CHANGE24HOUR"]
         change_str = "%.2f" % abs(change)
     return jsonify({'price': price_str, 'low': low_str, 'high': high_str, 'change': change_str, 'decrease': change<0})
